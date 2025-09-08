9761fb6b9485c4a73016182ed910c7b3e324bb54
secreto2111 <danicbrredessociais@gmail.com>
2025-09-08 09:15:13 -0300

    feat: atualizando o config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: { unoptimized: true },   // se usar <Image/>
-  basePath: '/Feliz-Aniversario',       // se o site for em https://usuario.github.io/NOME_DO_REPO
+  basePath: '/Feliz-Aniversario',  // se o site for em https://usuario.github.io/NOME_DO_REPO
 };
 module.exports = nextConfig;
